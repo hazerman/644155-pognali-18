11d5d35a91880c41dc819142a39698e0392ea9ba
hazerman <49827421+hazerman@users.noreply.github.com>
2019-12-04 00:29:07 +0500

    Обновляет скрипт выпадающего списка стран

--- source/js/filter-country-drop.js
+++ source/js/filter-country-drop.js
@@ -1,12 +1,28 @@
 var dropCountryWrapper = document.querySelector(".plan__countries-wrapper");
 var dropBtns = dropCountryWrapper.querySelectorAll(".plan__country-button");
-
-console.log(dropCountryWrapper);
-console.log(dropBtns);
-
+var countryList = dropCountryWrapper.querySelector(".plan__filter-country");
+var countryListLetterBtns = countryList.querySelectorAll(".filter-country__link-letter");
 
 dropBtns[2].addEventListener("click", function (evt) {
   evt.preventDefault();
 
   dropBtns[2].classList.toggle("plan__country-button--show");
 })
+
+var changeDropList = function (btn) {
+  btn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    for (var i = 0; i < countryListLetterBtns.length; i++) {
+      if (countryListLetterBtns[i].classList.contains("filter-country__link-letter--current")) {
+        countryListLetterBtns[i].classList.remove("filter-country__link-letter--current");
+      }
+    }
+
+    btn.classList.add("filter-country__link-letter--current");
+  })
+}
+
+for (var i = 0; i < countryListLetterBtns.length; i++) {
+  changeDropList(countryListLetterBtns[i]);
+}
