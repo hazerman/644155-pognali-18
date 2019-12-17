6ed9e3371dd11bf3eef90dd7d5b482e253a5d809
hazerman <49827421+hazerman@users.noreply.github.com>
2019-12-17 14:35:52 +0500

    Подгоняет скрипт фильтров под тз

--- source/js/filter-form.js
+++ source/js/filter-form.js
@@ -2,6 +2,11 @@ var filtersFormWrapper = document.querySelector(".filters__category-list");
 var filtersFormBtns = filtersFormWrapper.querySelectorAll(".filters__button-toggle");
 var filtersFormCategories = filtersFormWrapper.querySelectorAll(".filters__wrapper");
 
+for (var i = 0; i < filtersFormBtns.length - 1; i++) {
+  filtersFormCategories[i].classList.remove("filters__wrapper--show");
+  filtersFormBtns[i].classList.remove("filters__button-toggle--expanded");
+}
+
 var touchCategory = function(btn, wrap) {
   btn.addEventListener("click", function (evt) {
     btn.classList.toggle("filters__button-toggle--expanded");
